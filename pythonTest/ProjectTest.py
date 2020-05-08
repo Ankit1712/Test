41fc2bd8a5c2d059d06ffea9a660f09a076372a1
Ankit1712 <64196775+Ankit1712@users.noreply.github.com>
2020-05-09 01:43:12 +0530

    Create ProjectTest.py

---
+++ pythonTest/ProjectTest.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.keys import Keys
+options = Options();
+options.add_argument("--headless");
+driver = webdriver.Firefox(executable_path='/usr/local/bin/geckodriver',options=options)
+driver.get("http://192.168.0.10:8080/hellosample/index.html")
+assert "Hello Index" in driver.title
+driver.close()
